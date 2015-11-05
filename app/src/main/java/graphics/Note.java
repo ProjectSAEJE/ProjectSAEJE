28c554e15c99da36dbc71c7f6ea1195332470275
austinnash <nashau01@luther.edu>
2015-11-05 17:08:48 -0600

    Created a music package in app/java containing a Note class.

--- app/src/main/java/graphics/Note.java
+++ app/src/main/java/graphics/Note.java
@@ -5,10 +5,9 @@ package graphics;
  */
 
 import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;  // May or may not be pseudocode depending on what Drawable actually is, might be too abstract
 import android.media.Image;
 
-public class Note { // As above, may be better to extend from an subclass of Drawable that supports images (for the notes, quarter, eighth, etc.)
+public class Note {
 
     public float x, y; //for drawing to the 2D canvas in the UI/display
     public String type; //"whole", "half", "quarter", "eighth", "sixteenth"...
