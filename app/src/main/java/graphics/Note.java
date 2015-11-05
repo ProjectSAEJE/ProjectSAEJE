fb60e1a339941036f3e00d0f2314854765e6a072
woodev01 <woodev01@luther.edu>
2015-11-05 16:07:32 -0600

    Evan's Commit:     - Updated Staffer

--- app/src/main/java/graphics/Note.java
+++ app/src/main/java/graphics/Note.java
@@ -6,21 +6,22 @@ package graphics;
 
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;  // May or may not be pseudocode depending on what Drawable actually is, might be too abstract
+import android.media.Image;
 
-public class Note extends Drawable { // As above, may be better to extend from an subclass of Drawable that supports images (for the notes, quarter, eighth, etc.)
+public class Note { // As above, may be better to extend from an subclass of Drawable that supports images (for the notes, quarter, eighth, etc.)
 
     public float x, y; //for drawing to the 2D canvas in the UI/display
     public String type; //"whole", "half", "quarter", "eighth", "sixteenth"...
-    public image Image; //An image of this type of note
+    public Image image; //An image of this type of note
 
     public Note() {
     }
 
-    public Note(float x, float y, String type) {
-        this.x = x;
+    public Note(float y, String type) {
+        this.x = 1;
         this.y = y;
         this.type = type;
-        this.image = Image(x, y, imageFileFor(type)); //the imageFor function would need to have access to a data structure (dictionary?) that maps strings -> images
+        this.image = R.drawable.dial; //the imageFor function would need to have access to a data structure (dictionary?) that maps strings -> images
     }
 
     @Override
