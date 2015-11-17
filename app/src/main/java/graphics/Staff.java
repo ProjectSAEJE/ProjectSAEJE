740c5bb0025358e3f81ae9d2b9599c2e9afe1db3
woodev01 <woodev01@luther.edu>
2015-11-17 16:03:07 -0600

    Evan's Commit:     - Updates to Staff.java and Note.java     - Includes a dialog box     - Save now from the Note class Not the Staff class     - Saves all to the same file

--- app/src/main/java/graphics/Staff.java
+++ app/src/main/java/graphics/Staff.java
@@ -1,7 +1,11 @@
 package graphics;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import graphics.Note;
@@ -27,39 +31,37 @@ public class Staff extends Activity {
         this.notes = notes;
     }
 
+    private String m_Text;
 
-    private String NoteToString(){
-        return String;
-    }
+    private void NoteToString(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
 
-    //saveData = data to be saved, saejeData = name of the internal file
-    private void internalSave(String saveData, String saejeData){
-        FileOutputStream fos = null;
-        try{
-            fos = openFileOutput(saejeData, this.MODE_PRIVATE);
-            fos.write(saveData.getBytes());
-            fos.close();
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
 
-            preferencesEditor.putBoolean("isInternal", true).commit();
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
 
-            buttonShowInternal.setVisibility(View.VISIBLE);
-            buttonShowInternal.setOnClickListener(this);
+        builder.show();
 
-        } catch (IOException e){
-            Toast.makeText(this,"There's a problem saving to the internal file",Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
+        for (Note aNote: this.notes) {
+            aNote.save(m_Text);
         }
-
     }
-    /*
-    String filename = "saeje_file";
-    String string = "TESTING"; //replace with staff object
-    FileOutputStream outputStream;
 
-    try{
-        outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-        outputStream.write(string.getBytes());
-        outputStream.close();
-    } catch(Exception e){e.printStackTrace();} //change the Exception
-    */
 }
