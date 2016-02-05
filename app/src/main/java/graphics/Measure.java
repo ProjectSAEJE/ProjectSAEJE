b7fed431c50711204c68a5230c04d15a3a888cfa
austinnash <nashau01@luther.edu>
2016-02-04 20:19:53 -0600

    Measure class updated to have an x and y position set implicitly by the measure number. In order for this to work, the staff image's width must be accessible somehow.

--- app/src/main/java/graphics/Measure.java
+++ app/src/main/java/graphics/Measure.java
@@ -1,19 +1,24 @@
 package graphics;
 
+import android.graphics.Bitmap;
+
 import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * Created by austinnash on 12/6/15.
  */
 
 public class Measure {
-    private int number;
+    private int number; //starts as 1 not 0 per music composition norms
     private ArrayList<Note> notes;
+    private float xPos;
+    private float yPos;
+    private Bitmap staffImage; //THIS IS CURRENTLY A DUMMY VARIABLE. Rework the staff object so that the staff image is actually a bitmap object, not simply the background specified in xml?
 
     //Other possible fields:
     //private Activity activity?
     //private View view?
-    //private Vector position?
     //private Bitmap staffPNG?
 
     public Measure() {}
@@ -21,6 +26,8 @@ public class Measure {
     public Measure(int number, ArrayList<Note> notes) {
         this.number = number;
         this.notes = notes;
+        xPos = staffImage.getWidth() * (number - 1);
+        yPos = 0;
     }
 
     public void addNote(Note note) {
