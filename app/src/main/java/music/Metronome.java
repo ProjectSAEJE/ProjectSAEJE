c1c62a6f78dfa1d1202d29eff115cb2a3c8cb8e0
austinnash <nashau01@luther.edu>
2016-02-11 17:19:22 -0600

    Updated Metronome class.

--- app/src/main/java/music/Metronome.java
+++ app/src/main/java/music/Metronome.java
@@ -4,4 +4,11 @@ package music;
  * Created by austinnash on 2/11/16.
  */
 public class Metronome {
+    private int tempo;
+    private int[] timeSignature;
+
+    public Metronome(int tempo, int[] timeSignature) {
+        this.tempo = tempo;
+        this.timeSignature = timeSignature;
+    }
 }
