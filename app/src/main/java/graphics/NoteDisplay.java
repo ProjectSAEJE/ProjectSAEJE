69005a94529bc1100cfcae4276af1b07bcb289d3
woodev01 <woodev01@luther.edu>
2016-02-16 16:25:30 -0600

    Evan's Commit:
    - Added Drawable Functions:
         - NoteDisplay
         - StaffDisplay
    - Made significant changes to:
         - Note
         - Staff
         - Measure
         - DrawingView
    
    - Use Caution before pulling

--- app/src/main/java/graphics/NoteDisplay.java
+++ app/src/main/java/graphics/NoteDisplay.java
@@ -2,6 +2,8 @@ package graphics;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
 
 import com.example.woodev01.projectsaeje.R;
@@ -17,5 +19,25 @@ public class NoteDisplay extends Drawable {
 
         //Creates a ScaledBitmap to make quarter note a certain size
         this.image = Bitmap.createScaledBitmap(b, 300, 300, false);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
     }
 }
--- app/src/main/java/graphics/StaffDisplay.java
+++ app/src/main/java/graphics/StaffDisplay.java
@@ -1,10 +1,34 @@
 package graphics;
 
-public class StaffDisplay {
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+public class StaffDisplay extends Drawable {
 
     public StaffDisplay(){};
 
     public static void displayStaff(android.graphics.Canvas canvas){
 
     };
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
 }
