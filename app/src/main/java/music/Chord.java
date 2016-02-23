b9ea7b59b892e3791fd4a5038f8351e3cf180606
jorgja02 <jorgja02@luther.edu>
2016-02-23 15:03:40 -0600

    Added a couple more chord types to Chord.java

--- app/src/main/java/music/Chord.java
+++ app/src/main/java/music/Chord.java
@@ -32,4 +32,24 @@ public class Chord {
 
     }
 
+    public void setToMajor7Chord(){
+        int majThirdNum = root.getVal() + 4;
+        int fifthNum = root.getVal() + 7;
+        int seventhNum = root.getVal() + 10;
+        intervalNotes.set(0, new Note(majThirdNum));
+        intervalNotes.set(1, new Note(fifthNum));
+        intervalNotes.set(2, new Note(seventhNum));
+
+    }
+
+    public void setToMinor7Chord(){
+        int minThirdNum = root.getVal() + 3;
+        int fifthNum = root.getVal() + 7;
+        int seventhNum = root.getVal() + 10;
+        intervalNotes.set(0, new Note(minThirdNum));
+        intervalNotes.set(1, new Note(fifthNum));
+        intervalNotes.set(2, new Note(seventhNum));
+
+    }
+
 }
