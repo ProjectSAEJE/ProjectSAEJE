aaae90d9c52204c088c1e21aeed5c6b6427deda1
woodev01 <woodev01@luther.edu>
2016-02-25 16:04:14 -0600

    Evan's Commit:
    - Added Drawable Functions:
         - NoteDisplay
         - StaffDisplay
    - Made significant changes to:
         - Note
         - Staff
         - Measure
         - DrawingView
    
    - Use Caution before pulling

--- app/src/main/java/graphics/StaffDisplay.java
+++ app/src/main/java/graphics/StaffDisplay.java
@@ -3,14 +3,22 @@ package graphics;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
+import music.Measure;
+import music.Staff;
 
 public class StaffDisplay extends Drawable {
 
     public StaffDisplay(){};
 
     public static void displayStaff(android.graphics.Canvas canvas){
-
+        chooseMeasures();
     };
+
+
+    public Staff chooseMeasures() {
+        Staff s = new Staff();
+        return s;
+    }
 
     @Override
     public void setAlpha(int alpha) {
