4e80e6cdb37c9b88388cb6adf1650a31a1c85e4c
jorgja02 <jorgja02@luther.edu>
2016-03-29 15:59:54 -0500

    Added ChordBuilder.java for future use.

--- app/src/main/java/music/controller/ChordAnalyzer.java
+++ app/src/main/java/music/controller/ChordAnalyzer.java
@@ -1,14 +1,16 @@
 package music.controller;
 
+
+import java.util.*;
+import music.model.*;
+
 public class ChordAnalyzer {
 
-    /*
-    public Chord(ArrayList<Note>, RhythmicVal){
+    public Note note;
+    public Chord chord;
 
-        //stuff TBD
-        //
+    public int noteVal = (int)note.tonalValue;
 
-    }
-    */
+    //stuff TBD
 
 }
---
+++ app/src/main/java/music/controller/ChordBuilder.java
@@ -0,0 +1,9 @@
+package music.controller;
+
+import java.util.*;
+import music.model.*;
+
+public class ChordBuilder {
+
+    //takes ChordAnalyzer & builds chords on the screen
+}
